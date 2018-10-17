47c562fe73bcd385083de881bf95e32ba3bacfe0
Holrik <christophe.caubet@etu.u-bordeaux.fr>
2018-10-17 14:19:46 +0200

    Remove unnecessary voxels during the reading of the file

--- src/voxel_mesh.cpp
+++ src/voxel_mesh.cpp
@@ -5,6 +5,8 @@
 #include <cmath>
 
 void VoxelMesh::loadFromPGM3D(const std::string &file_name) {
+  using uint = unsigned int;
+  using ushort = unsigned short;
   std::fstream f{file_name, std::ios::in};
   if (!f.is_open()) {
     std::cerr << "File " << file_name << " not found !\n";
@@ -21,9 +23,11 @@ void VoxelMesh::loadFromPGM3D(const std::string &file_name) {
 
   unsigned int size = column * line * depth;
   intensity_array.reserve(size);
-  for (uint i = 0; i < depth; ++i) {
+  std::vector<Vec3_base<ushort>> voxels_position;
+  std::vector<ushort> voxels_intensity;
+  for (uint k = 0; k < depth; ++k) {
     for (uint j = 0; j < line; ++j) {
-      for (uint k = 0; k < column; ++k) {
+      for (uint i = 0; i < column; ++i) {
         Value v;
         f >> v;
         if (f.fail()) {
@@ -31,20 +35,16 @@ void VoxelMesh::loadFromPGM3D(const std::string &file_name) {
           std::exit(1);
         }
         if (v != 0) {
-          intensity_array.emplace_back((float)v / (float)max_value);
-          Vec3 p = Vec3({-0.5f+(float)k/(float)column, 
-                      -0.5f+(float)j/(float)line, 
-                      -0.5f+(float)i/(float)depth});
-
-          position_array.emplace_back(p);
-
-          //printf("%f,%f,%f\n", p.x, p.y, p.z);
+          voxels_intensity.emplace_back(v);
+          Vec3_base<ushort> p = Vec3_base<ushort>({i, j, k});
+          //printf("%d,%d,%d\n", p.x, p.y, p.z);
+          voxels_position.emplace_back(p);
         }
       }
     }
   }
 
-  /*std::vector<Vec3> neighbors;
+  std::vector<Vec3> neighbors;
   neighbors.emplace_back(Vec3{-1, 0, 0});
   neighbors.emplace_back(Vec3{ 1, 0, 0});
   neighbors.emplace_back(Vec3{ 0,-1, 0});
@@ -54,52 +54,46 @@ void VoxelMesh::loadFromPGM3D(const std::string &file_name) {
 
   auto within = [](int a, int val, int b) { return a <= val && val < b; };
 
-  auto are_equals = [](float a, float b) { return std::abs(a - b) < 0.00001f; };
-
   std::vector<int> to_delete;
-
-  std::cout << position_array.size() << std::endl;
-  for (int actual_vox = 0; actual_vox < position_array.size(); ++actual_vox) {
-    int i0 = position_array.at(actual_vox).x;
-    int j0 = position_array.at(actual_vox).y;
-    int k0 = position_array.at(actual_vox).z;
+  std::cout << voxels_position.size() << std::endl;
+  for (int actual_vox = 0; actual_vox < voxels_position.size(); ++actual_vox) {
+    int i0 = voxels_position.at(actual_vox).x;
+    int j0 = voxels_position.at(actual_vox).y;
+    int k0 = voxels_position.at(actual_vox).z;
     uint res = 0;
     for (uint indice = 0; indice < neighbors.size(); ++indice) {
-//std::cout << "res:" << res << " ind:" << indice << std::endl;
       if (res != indice) break;
       int i = i0 + neighbors.at(indice).x;
       int j = j0 + neighbors.at(indice).y;
       int k = k0 + neighbors.at(indice).z;
       if (!within(0, i, depth) || !within(0, j, line) || !within(0, k, column)) {
         ++res;
-        continue; // res++;
+        continue;
       }
 
-      for (uint vox = 0; vox < position_array.size(); ++vox) {
+      for (uint vox = 0; vox < voxels_position.size(); ++vox) {
         // UTILISER FIND() PLUTOT http://www.cplusplus.com/reference/algorithm/find/
-        if (are_equals(position_array.at(vox).x, (float)i)
-          && are_equals(position_array.at(vox).y, (float)j)
-          && are_equals(position_array.at(vox).z, (float)k))
+        if (voxels_position.at(vox).x == i
+          && voxels_position.at(vox).y == j
+          && voxels_position.at(vox).z == k)
         {
-          if (are_equals(intensity_array.at(actual_vox), intensity_array.at(vox))) {
+          if (voxels_intensity.at(actual_vox) == voxels_intensity.at(vox)) {
             ++res;
           }
           break;
         }
       }
     }
-//std::cout << res << std::endl;
-    if (res == neighbors.size()) {
-//std::cout << "test" << std::endl;
-      to_delete.emplace_back(actual_vox);
+
+    if (res != neighbors.size()) {
+      intensity_array.emplace_back((float)voxels_intensity.at(actual_vox) / (float)max_value);
+      Vec3 p = Vec3({-0.5f+(float)i0/(float)column, 
+                  -0.5f+(float)j0/(float)line, 
+                  -0.5f+(float)k0/(float)depth});
+      position_array.emplace_back(p);
     }
   }
-
-  for(int i = to_delete.size() - 1; i >= 0; --i) {
-    position_array.erase(position_array.begin() + to_delete.at(i));
-    intensity_array.erase(intensity_array.begin() + to_delete.at(i));
-  }
-  std::cout << position_array.size() << std::endl;*/
+  std::cout << position_array.size() << std::endl;
 }
 
 /*
